96eaef354e7b16efafd2e7be33cda86fbfc1d14d
igor-music <igormusich@gmail.com>
2016-03-29 21:25:55 -0400

    Initial commit

---
+++ SelfHost/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin.Hosting;
+using Serilog;
+using System;
+
+namespace SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Windows Authentication Service";
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.LiterateConsole()
+                .CreateLogger();
+
+            using (WebApp.Start<Startup>("https://localhost:44350"))
+            {
+                Console.WriteLine("running...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ WebHost/Startup.cs
@@ -0,0 +1,24 @@
+﻿using IdentityModel;
+using IdentityServer.WindowsAuthentication.Configuration;
+using Microsoft.Owin;
+using Owin;
+using System.Linq;
+
+[assembly: OwinStartup(typeof(WebHost.Startup))]
+
+namespace WebHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWindowsAuthenticationService(new WindowsAuthenticationOptions
+            {
+                IdpRealm = "urn:idp",
+                IdpReplyUrl = "https://localhost:44333/core/was",
+
+                SigningCertificate = X509.LocalMachine.My.SubjectDistinguishedName.Find("CN=sts").First()
+            });
+        }
+    }
+}
